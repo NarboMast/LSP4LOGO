589c04ccd651afbe43c88679308931c122a585dc
GitHub Action <action@github.com>
2026-04-13 07:50:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/narbomast/lsp4logo/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/narbomast/lsp4logo/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.narbomast.lsp4logo.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
